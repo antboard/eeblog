31a42d4fad1dcd7bd49378b4cdcd6d5d091fb426
50100112 <icecut@qq.com>
2019-06-10 23:03:39 +0800

    +模板,页面渲染

--- handle/web.go
+++ handle/web.go
@@ -4,13 +4,57 @@ import (
 	"net/http"
 
 	"github.com/antboard/eeblog/model"
-
 	"github.com/gin-gonic/gin"
 )
+
+// Tags 顶部标签
+type Tags struct {
+	Active bool
+	Tag    string
+	URL    string
+}
+
+// BlogSummary 博文摘要
+type BlogSummary struct {
+	Title   string
+	Summary string
+	URL     string
+}
+
+// MainPage 首页渲染数据
+type MainPage struct {
+	Title      string
+	Project    string
+	Tags       []*Tags
+	Bigtitle   string
+	Bitsummary string
+	Blogs      []*BlogSummary
+}
 
 // Index ...
 func Index(c *gin.Context) {
 	vbs := model.GetResentBlog(0)
-	c.JSON(http.StatusOK, vbs)
+	mp := &MainPage{}
+	mp.Title = "eeblog"
+	mp.Project = "EEBLOG"
+	mp.Tags = make([]*Tags, 0, 10)
+	tg := new(Tags)
+	tg.Active = true
+	tg.Tag = "首页"
+	tg.URL = "/"
+	mp.Tags = append(mp.Tags, tg)
+	mp.Bigtitle = "Hello EEBLOG"
+	mp.Bitsummary = "这就是一个比较好的电子工程学博客"
+	for _, v := range vbs {
+		blog := new(BlogSummary)
+		blog.Title = v.Title
+		blog.Summary = v.Summary
+		blog.URL = "/eeblog/" + v.ID
+		mp.Blogs = append(mp.Blogs, blog)
+	}
+
+	c.HTML(http.StatusOK, "index.tmpl", mp)
+	// c.HTML(http.StatusOK, "index.tmpl", gin.H{"title": "测试", "project": "EEBLOG", "tags": []gin.H{gin.H{"active": true, "tag": "tag", "URL": "/"}}})
+	// c.JSON(http.StatusOK, vbs)
 	// c.String(http.StatusOK, "...")
 }
