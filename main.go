e6ecf6591d1424d57ddeee214d230b47435479c2
50100112 <icecut@qq.com>
2019-08-24 22:01:55 +0800

    change to gin session

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/antboard/eeblog/handle"
 	_ "github.com/antboard/eeblog/mdex"
 	"github.com/antboard/eeblog/model"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
@@ -19,14 +21,18 @@ func main() {
 	viper.BindEnv("dbuser")
 	viper.BindEnv("dbpassword")
 	log.SetFlags(log.Lshortfile)
+
 	model.Engine = model.GetDBEngine()
 	err := model.Engine.Sync2(new(model.Blog))
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
+
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
+	store := cookie.NewStore([]byte("EEBLOG"))
+	r.Use(sessions.Sessions("eeblog", store))
 	r.LoadHTMLGlob("templates/*")
 	r.Static("/js/", "./web/js/")
 	r.Static("/css/", "./web/css/")
